7f3dfba0202a84bc9e643db382f46d88ea59e1cb
Nareh Bairagai <139035017+Naresh-021@users.noreply.github.com>
2025-07-31 17:15:58 +0530

    Update LargestNumber.java
    added Comments for the code

--- LargestNumber.java
+++ LargestNumber.java
@@ -17,16 +17,20 @@ public class LargestNumber {
     }
 
     public static void main(String[] args) {
+ // This are the Inputs declared        
 //        int [] array = {1,3,445,65};
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the size of an Array");
         int  number = sc.nextInt();
+        
         int[] arrayInput = new int[number];
         System.out.println("Enter Elements in the Array");
         for (int i = 0; i < number; i++) {
+            // Taking an Inputs from the User and storing in an variable arrayInput
             arrayInput[i] = sc.nextInt();
         }
+        // This For Loop used to print the array element what ever stored in an Array
         for(int num : arrayInput){
             System.out.print(num+" ");
         }
